7859cfb7abdb689aa075f657cf3cabb3f9301bf7
Carlos Huamacto <carlos.huamacto@srburns.pe>
2023-06-26 02:10:14 -0500

    #se hizo el index de la pagina alojado en otro repo

--- src/js/pages/proyectos/ecoshop/index.js
+++ src/js/pages/proyectos/ecoshop/index.js
@@ -1,2 +1,13 @@
 import 'normalize.css'
 import '../../../../scss/pages/proyectos/ecoshop/index.scss';
+import './slick/slick'
+
+$('.remanso-hero').slick({
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
